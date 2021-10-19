43b05b0b4bc38eae5c2647d35e2ba74b65355bb3
CodingTaddo <gio.tad@outlook.it>
2021-10-19 10:27:48 +0200

    TODO: correct switch case so that it returns "Not valid" when an invalid  vote in inserted

---
+++ conta_favorevoli_switch.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int favorevoli, contrari, astenuti;
+    char voto;
+
+    favorevoli = 0;
+    contrari = 0;
+    astenuti = 0;
+
+    while ((voto = getchar()) != EOF)
+    {
+        
+        while (getchar() != '\n');
+
+        switch (voto)
+        {
+            case 'f':
+            case 'F':
+            case 's':
+            case 'S':
+            case 'y':
+            case 'Y':
+                favorevoli++;
+                break;
+            
+            case 'c':
+            case 'C':
+            case 'n':
+            case 'N':
+                contrari++;
+                break;
+            
+            case 'a':
+            case 'A':
+            case '-':
+                astenuti++;
+                break;
+            
+            default:
+                printf("Voto non valido");
+                break;
+
+        }
+        
+    }
+
+    printf("Favorevoli: %d\n", favorevoli);
+    printf("Contrari: %d\n", contrari);
+    printf("Astenuti: %d\n", astenuti);
+}
