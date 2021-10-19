a9ba41e8bc0c807ca82bc9dbde9cb91dfc8b4692
CodingTaddo <gio.tad@outlook.it>
2021-10-19 09:43:43 +0200

    Applied some changes to the template file creation

--- create_default.sh
+++ create_default.sh
@@ -3,12 +3,15 @@
 touch "default.c";
 echo '#include <stdio.h>
 #include <stdlib.h>
+
 int main()
 {
-    int n, contatore, n_fattoriale;
+    int n, contatore;
     contatore = 0;
+
     printf("Inserire n: ");
     scanf("%d", &n);
+    
 }' > "default.c";
 
 if [ "$1" == "" ]
